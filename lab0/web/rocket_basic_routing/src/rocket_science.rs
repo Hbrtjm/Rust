9a14d14f1e7948385f750864168d7e03688ae8c2
Hbrtjm <hubi.hey@vp.pl>
2025-03-12 22:16:14 +0100

    Fixed web server issues

--- lab0/web/rocket_basic_routing/src/rocket_science.rs
+++ lab0/web/rocket_basic_routing/src/rocket_science.rs
@@ -1,7 +1,8 @@
 #[macro_use] extern crate rocket;
-use std::io::{self,Read};
-use std::fs::File;
-use rocket::{response::content::RawHtml};
+// use std::io::{self,Read};
+// use std::fs::File;
+// use rocket::{response::content::RawHtml};
+use rocket_dyn_templates::{Template, context};
 
 #[get("/")]
 fn index() -> &'static str {
@@ -9,12 +10,13 @@ fn index() -> &'static str {
 }
 
 #[get("/login")]
-fn login() -> Result<String, io::Error> {
-    let mut file = File::open("static/login.html")?;
-    let mut contents = String::new();
-    file.read_to_string(&mut contents)?;
-    // RawHtml(contents) as Result<std::string::String, std::io::Error>
-    Ok(contents);
+fn login() -> Template { // -> Result<String, io::Error>{
+//    let mut file = File::open("static/login.html")?;
+//    let mut contents = String::new();
+//    file.read_to_string(&mut contents)?;
+//    // RawHtml(contents) as Result<std::string::String, std::io::Error>
+//    Ok(contents)
+Template::render("login", context! { field: "value" })
 }
 
 #[launch]
@@ -22,5 +24,6 @@ fn rocket() -> _ {
     rocket::build()
         .mount("/", routes![index])
         .mount("/", routes![login])
+        .attach(Template::fairing())
 }
 
